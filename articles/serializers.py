798aabac2ad49f3bf263c3e52574749e9b358103
Akiyo Fidel <akiyofidel@gmail.com>
2020-07-28 22:27:39 +0300

    added ability to post article

--- articles/serializers.py
+++ articles/serializers.py
@@ -1,11 +1,17 @@
 from rest_framework import serializers
 
+from .models import Article, Author
+
 class ArticleSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=120)
     description = serializers.CharField()
     body = serializers.CharField()
     created_date = serializers.DateTimeField()
     published_date = serializers.DateTimeField()
+    author_id = serializers.IntegerField()
+
+    def create(self, validated_data):
+        return Article.objects.create(**validated_data)
 
 class AuthorSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=120)
--- articles/views.py
+++ articles/views.py
@@ -12,9 +12,27 @@ class AuthorView(APIView):
         authors = Author.objects.all()
         serializer = AuthorSerializer(authors, many=True)
         return Response({"authors": serializer.data})
+    
+    def post(self, request):
+        author = request.data.get('article')
+
+        #create article form above data
+        serializer = AuthorSerializer(data=author)
+        if serializer.is_valid(raise_exception=True):
+            author_saved = serializer.save()
+        return Response({"Success": "Author '{}' created successfully".format(author_saved.name)})
 
 class ArticleView(APIView):
     def get(self, request):
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
         return Response({"articles": serializer.data})
+
+    def post(self, request):
+        articles = request.data.get('articles')
+
+        #create article form above data
+        serializer = ArticleSerializer(data=articles)
+        if serializer.is_valid(raise_exception=True):
+            article_saved = serializer.save()
+        return Response({"Success": "Article '{}' created successfully".format(article_saved.title)})
